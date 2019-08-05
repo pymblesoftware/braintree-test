2508ca2c05962295e26188868632a8f26a38ede7
Pymblesoftware <dev1@pymblesoftware.com>
2019-08-05 13:47:28 +1000

    Integrated some code from the sample

--- Test3/ViewController.h
+++ Test3/ViewController.h
@@ -7,9 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "BTPaymentMethodNonce.h"
 
 @interface ViewController : UIViewController
 
+
+@property (nonatomic, weak) void (^progressBlock)(NSString *newStatus);
+@property (nonatomic, weak) void (^completionBlock)(BTPaymentMethodNonce *paymentMethodNonce);
+@property (nonatomic, weak) void (^transactionBlock)(void);
 
 @end
 
